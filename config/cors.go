5b1601cbac9a936f1b3196a9506d5dba554e3a81
MhmmdMallq <Kamalputra1177@gmail.com>
2026-06-22 15:40:26 +0700

    push wew

--- config/cors.go
+++ config/cors.go
@@ -3,6 +3,7 @@ package config
 var allowedOrigins = []string{
 	"http://localhost:5173",
 	"https://be-latihan-production-89a4.up.railway.app",
+	"my-fe-one.vercel.app",
 }
 
 func GetAllowedOrigins() []string {
